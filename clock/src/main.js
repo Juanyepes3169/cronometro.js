fee40decf0bfaf35d507e54463f506caa540a487
Juanyepes3169 <161142285+Juanyepes3169@users.noreply.github.com>
2024-02-24 09:58:16 -0500

    Se cargo la carpeta clock

---
+++ clock/src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/c6b9JwUCtyDXVV6g/scene.splinecode');
